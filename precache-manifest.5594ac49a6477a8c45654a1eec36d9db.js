14fd8050d83fb67d650ba1dbb4730ba883deebd9
Mikhail Lapshin <lapshinmr@gmail.com>
2020-03-26 15:53:23 +0400

    Updates

---
+++ precache-manifest.5594ac49a6477a8c45654a1eec36d9db.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9e8e3ca5f2eb2020df9e234879081f2",
+    "url": "/404.html"
+  },
+  {
+    "revision": "39228c11734ecec3bccd8b294f11dc70",
+    "url": "/CNAME"
+  },
+  {
+    "revision": "539664b289ba82818399",
+    "url": "/css/app.e0bc22dd.css"
+  },
+  {
+    "revision": "731961f197c1ef7a40d7",
+    "url": "/css/chunk-vendors.bc2bf8cc.css"
+  },
+  {
+    "revision": "2b2b401a03cdd4f6244397cfeaee985a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "539664b289ba82818399",
+    "url": "/js/app.15bbf1ba.js"
+  },
+  {
+    "revision": "731961f197c1ef7a40d7",
+    "url": "/js/chunk-vendors.68e3ff27.js"
+  },
+  {
+    "revision": "687e463d1c566435e06006941f02ed14",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
